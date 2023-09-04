7b889214fa00e27ce2a21c1414163eee0e2d4834
Felipe Tofollo <felipetofollo11@gmail.com>
2023-09-04 11:49:15 -0300

    Adicionado responsividade do meenu lateral

--- src/shared/components/menu-lateral/MenuLateral.tsx
+++ src/shared/components/menu-lateral/MenuLateral.tsx
@@ -1,6 +1,7 @@
 
 import {  Avatar, Divider, Drawer, Icon, List, ListItemButton, ListItemIcon, ListItemText, useMediaQuery, useTheme } from '@mui/material';
 import { Box } from '@mui/system';
+import { useDrawerContext } from '../../contexts';
 
 
 interface IMenuLateral {
@@ -10,11 +11,14 @@ interface IMenuLateral {
 
 export const MenuLateral: React.FC<IMenuLateral>  = ({ children }) => {
     const theme = useTheme();
+    const smDown = useMediaQuery(theme.breakpoints.down('sm'));
+
+    const {isDrawerOpen, toggleDrawerOpen } = useDrawerContext();
     
 
     return (
         <>
-        <Drawer variant='permanent'>
+        <Drawer open={isDrawerOpen} variant={smDown ? 'temporary' : 'permanent'} onClose={toggleDrawerOpen}>
             <Box width={theme.spacing(28)} height="100%" display="flex" flexDirection="column" >
 
             <Box width="100%" height={theme.spacing(20)} display="flex" alignItems="center" justifyContent="center">
@@ -42,7 +46,7 @@ export const MenuLateral: React.FC<IMenuLateral>  = ({ children }) => {
         
         </Drawer>
 
-         <Box height="100vh" marginLeft={theme.spacing(28)}>
+         <Box height="100vh" marginLeft={smDown ? 0 :theme.spacing(28)}>
          {children}
          </Box>
             
