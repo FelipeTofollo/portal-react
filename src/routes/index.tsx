8297ce341728d55678334c5c7928b0d3f8e4a919
Felipe Tofollo <felipetofollo11@gmail.com>
2023-09-05 16:33:20 -0300

    criando layout base para paginas

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,12 @@
-import { Button } from '@mui/material';
+
 import { Routes, Route, Navigate } from 'react-router-dom';
 
 import {  useDrawerContext } from '../shared/contexts';
 import { useEffect } from 'react';
+import { Dashboard } from '../pages';
 
 export const AppRoutes = () => {
-    const {toggleDrawerOpen, setDrawerOptions} = useDrawerContext();
+    const { setDrawerOptions} = useDrawerContext();
 
     useEffect(() => {
         setDrawerOptions([
@@ -14,37 +15,16 @@ export const AppRoutes = () => {
                 icon: 'home',
                 path: '/pagina-inicial',
             },
-            {
-                label: 'Secretaria',
-                icon: 'star',
-                path: '/secretaria',
-            },
-            {
-                label: 'Sistemas',
-                icon: 'settingssuggest',
-                path: '/sistemas',
-            },
-            {
-                label: 'Und. de Ensino',
-                icon: 'school',
-                path: '/unidade-ensino',
-            },
-
-            {
-                label: 'Fale conosco',
-                icon: 'phoneiphone',
-                path: '/fale-conosco',
-            },
         ]);
     }, []);
 
     return(
         <Routes>
-            <Route path="/pagina-inicial" element={<Button variant='contained' color='primary' onClick={toggleDrawerOpen}>Toggle drawer</Button>} />
-            <Route path="/secretaria" element={<Button variant='contained' color='primary' onClick={toggleDrawerOpen}>Toggle drawer</Button>} />
+            <Route path="/pagina-inicial" element={<Dashboard />} />
+         {  /* <Route path="/secretaria" element={<Button variant='contained' color='primary' onClick={toggleDrawerOpen}>Toggle drawer</Button>} />
             <Route path="/sistemas" element={<Button variant='contained' color='primary' onClick={toggleDrawerOpen}>Toggle drawer</Button>} />
             <Route path="/unidade-ensino" element={<Button variant='contained' color='primary' onClick={toggleDrawerOpen}>Toggle drawer</Button>} />
-            <Route path="/fale-conosco" element={<Button variant='contained' color='primary' onClick={toggleDrawerOpen}>Toggle drawer</Button>} />
+    <Route path="/fale-conosco" element={<Button variant='contained' color='primary' onClick={toggleDrawerOpen}>Toggle drawer</Button>} /> */ }
 
             <Route path="*" element={<Navigate to="/pagina-inicial" />} />
         </Routes>
